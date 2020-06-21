6df83028d04f3e764a1a019add82404d7a343174
rubii9 <rubenpo167@gmail.com>
2020-06-21 14:22:45 +0200

    view error

--- Entregas/Front/src/router/index.js
+++ Entregas/Front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Error from "../views/Error.vue";
 
 Vue.use(VueRouter);
 
@@ -14,22 +15,24 @@ const routes = [
     path: "/login",
     name: "Login",
 
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+    component: () => import("../views/Login.vue"),
   },
   {
     path: "/register",
     name: "Register",
 
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Register.vue"),
+    component: () => import("../views/Register.vue"),
   },
   {
     path: "/about",
     name: "About",
 
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "*",
+    name: Error,
+    component: Error,
   },
 ];
 
