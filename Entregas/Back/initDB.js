832237ebaa4970e562a4f0985e5ce30d799e25d7
rubii9 <rubenpo167@gmail.com>
2020-06-26 14:53:26 +0200

    crear tabla comunidades

--- Entregas/Back/initDB.js
+++ Entregas/Back/initDB.js
@@ -99,6 +99,11 @@ async function main() {
       constraint fk_ratings_users_id foreign key (user_id) references users(id) ,
       constraint fk_ratings_spaces_id foreign key (space_id) references spaces(id)) ;
    `);
+  await connection.query(`
+  CREATE TABLE comunidades (
+  id_comunidad tinyint(4) NOT NULL,
+  nombre varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;`);
 
   // Create initial user
   const password = await bcrypt.hash(process.env.DEFAULT_ADMIN_PASSWORD, 10);
@@ -167,6 +172,30 @@ async function main() {
     await connection.query(`
     insert into incidents (comment,reserve_id) values ("me falla el monitor",1);
     `);
+
+    await connection.query(`
+    INSERT INTO comunidades (id_comunidad, nombre)
+    VALUES
+	    (1,'Andalucía'),
+      (2,'Aragón'),
+      (3,'Asturias, Principado de'),
+      (4,'Balears, Illes'),
+      (5,'Canarias'),
+      (6,'Cantabria'),
+      (7,'Castilla y León'),
+      (8,'Castilla - La Mancha'),
+      (9,'Catalunya'),
+      (10,'Comunitat Valenciana'),
+      (11,'Extremadura'),
+      (12,'Galicia'),
+      (13,'Madrid, Comunidad de'),
+      (14,'Murcia, Región de'),
+      (15,'Navarra, Comunidad Foral de'),
+      (16,'País Vasco'),
+      (17,'Rioja, La'),
+      (18,'Ceuta'),
+      (19,'Melilla');
+`);
   }
 
   console.log('Initial structure created');
